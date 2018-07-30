4719ae7e00380fcbccf321ec368b3a0d6366a480
Ross Hinman <hinman.ross@gmail.com>
2018-07-30 16:45:40 -0400

    Initial commit with working loop

---
+++ Lab_11_Movielist/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Movies.Library;
+
+namespace Lab_11_Movielist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> movieList = new List<Movie> { };
+            movieList.Add(new Movie("Up", "animated"));
+            movieList.Add(new Movie("Shrek", "animated"));
+            movieList.Add(new Movie("Tangled", "animated"));
+            movieList.Add(new Movie("Titanic", "drama"));
+            movieList.Add(new Movie("The Princess Bride", "drama"));
+            movieList.Add(new Movie("Saw", "horror"));
+            movieList.Add(new Movie("The Babadook", "horror"));
+            movieList.Add(new Movie("Pacific Rim", "scifi"));
+            movieList.Add(new Movie("Star Wars Episode I: The Phantom of the Opera", "scifi"));
+            bool repeat = true;
+            while (repeat == true)
+            {
+                Console.WriteLine("Enter a movie genre:\nAnimated\nDrama\nHorror\nSciFi");
+                string genre = Console.ReadLine().ToLower();
+                switch (genre)
+                {
+                    case "animated":
+                        foreach (Movie movie in movieList)
+                        {
+                            if (movie.GetGenre() == "animated")
+                            {
+                                Console.WriteLine(movie.GetTitle());
+                            }
+                        }
+                        break;
+                    case "drama":
+                        foreach (Movie movie in movieList)
+                        {
+                            if (movie.GetGenre() == "drama")
+                            {
+                                Console.WriteLine(movie.GetTitle());
+                            }
+                        }
+                        break;
+                    case "horror":
+                        foreach (Movie movie in movieList)
+                        {
+                            if (movie.GetGenre() == "horror")
+                            {
+                                Console.WriteLine(movie.GetTitle());
+                            }
+                        }
+                        break;
+                    case "scifi":
+                        foreach (Movie movie in movieList)
+                        {
+                            if (movie.GetGenre() == "scifi")
+                            {
+                                Console.WriteLine(movie.GetTitle());
+                            }
+                        }
+                        break;
+                        
+                }
+                Console.WriteLine("Would you like to continue? (y/n)");
+                string answer = Console.ReadLine().ToLower();
+                if (answer == "y")
+                {
+                    repeat = true;
+                }
+                else if (answer == "n")
+                {
+                    Console.WriteLine("Thank you!");
+                    repeat = false;
+                }
+            }                
+            
+        }      
+    }
+}
---
+++ Movies/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Movies.Library
+{
+    public class Movie
+    {
+        private string Genre;
+        private string Title;
+
+        public Movie(string title, string genre)
+        {
+            Genre = genre;
+            Title = title;
+        }
+        
+        public string GetTitle()
+        {
+            return Title;
+        }
+        public string GetGenre()
+        {
+            return Genre;
+        }
+    }
+
+}
